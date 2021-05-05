db2303d9da518dab48dc61949dfc85186bd6eccf
Frizlab <fload@me.com>
2021-05-05 11:32:41 +0200

    Add log for incoming requests in gRPC server for k8s

--- keyservice/k8s_server.go
+++ keyservice/k8s_server.go
@@ -24,10 +24,12 @@ func (s K8sServer) Version(ctx context.Context, req *k8skmsapi.VersionRequest) (
 	return &k8skmsapi.VersionResponse{Version: kmsapiVersion, RuntimeName: "sopsKMS", RuntimeVersion: version.Version}, nil
 }
 
-func (K8sServer) Decrypt(ctx context.Context, req *k8skmsapi.DecryptRequest) (*k8skmsapi.DecryptResponse, error) {
+func (s K8sServer) Decrypt(ctx context.Context, req *k8skmsapi.DecryptRequest) (*k8skmsapi.DecryptResponse, error) {
+	s.Log.Infof("Received request for Decrypt: %v", req)
 	return nil, status.Errorf(codes.Unimplemented, "method Decrypt not implemented")
 }
 
-func (K8sServer) Encrypt(ctx context.Context, req *k8skmsapi.EncryptRequest) (*k8skmsapi.EncryptResponse, error) {
+func (s K8sServer) Encrypt(ctx context.Context, req *k8skmsapi.EncryptRequest) (*k8skmsapi.EncryptResponse, error) {
+	s.Log.Infof("Received request for Decrypt: %v", req)
 	return nil, status.Errorf(codes.Unimplemented, "method Encrypt not implemented")
 }
